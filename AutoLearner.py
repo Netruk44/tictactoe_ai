c220ca995b02f80d4834732524deffd93a3a30a2
Daniel Perry <danieltylerperry@gmail.com>
2021-04-11 19:28:25 -0700

    Cleanup

--- AutoLearner.py
+++ AutoLearner.py
@@ -17,33 +17,25 @@ class AutoTTTLearner():
 
   def LearnFromSelf(self, iterations = 5):
     for _ in range(iterations):
-      finalBoard, moveList = TTTPlayer.PlayGameAgainstSelf(self.weights)
-      print(finalBoard, len(moveList))
-
-      learningExamples = Critic.generateTrainingExamplesFromGame(moveList)
-      print(f'Learning from {len(learningExamples)} examples last game.')
-      for i in learningExamples:
-        self.weights = WeightTrainer.iterateWeights(self.weights, i, learning_rate=self.learning_rate)
+      game = TTTPlayer.PlayGameAgainstSelf(self.weights)
+      self.LearnFromGame(game)
       
       print(self.weights)
   
   def LearnFromRandom(self, iterations = 5):
     for _ in range(iterations):
-      finalBoard, moveList = TTTPlayer.PlayGameAgainstRandom(self.weights)
-      print(finalBoard, len(moveList))
-
-      learningExamples = Critic.generateTrainingExamplesFromGame(moveList)
-      print(f'Learning from {len(learningExamples)} examples last game.')
-      for i in learningExamples:
-        self.weights = WeightTrainer.iterateWeights(self.weights, i, learning_rate=self.learning_rate)
+      game = TTTPlayer.PlayGameAgainstRandom(self.weights)
+      self.LearnFromGame(game)
       
       print(self.weights)
 
-  def LearnFromGame(self, game):
+  def LearnFromGame(self, game, showChange = False):
     finalBoard, moveList = game
     learningExamples = Critic.generateTrainingExamplesFromGame(moveList)
     print(f'Learning from {len(learningExamples)} examples last game.')
     old_weights = self.weights
     for i in learningExamples:
       self.weights = WeightTrainer.iterateWeights(self.weights, i, learning_rate=self.learning_rate)
-    print(f'Change: {[new - old for new, old in zip(self.weights, old_weights)]}')
+
+    if showChange:
+      print(f'Change: {[new - old for new, old in zip(self.weights, old_weights)]}')
